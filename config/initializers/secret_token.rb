989846af9c048900c42d7c08f1713050fda78156
vpablo <your.email@example.com>
2013-01-03 14:08:32 +0800

    bago

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fifthapp::Application.config.secret_token = '80b7f35e4f2522ef54f8f2d78ffdda227f52cb487640b422686bffd2d872a4cafcc4ced305b2fdb54097e063f0b1a37d79cc303624c2fc7588fabe55c8184fcb'
